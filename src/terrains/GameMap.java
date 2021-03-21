511667f8bc7b87d021b7020363031f852c5d67c7
codingberlin <berlin.stephan@gmail.com>
2021-03-21 20:44:53 +0100

    vertixes around fields

--- src/terrains/GameMap.java
+++ src/terrains/GameMap.java
@@ -9,13 +9,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class GameMap {
 
     private static final int SIZE = 100;
-    private static final float HEIGHT_FACTOR = 0.1F;
+    private static final int VERTEX_SIZE = SIZE + 1;
+    private static final float HEIGHT_FACTOR = 0.5F;
     private boolean depictionHasChanged = true;
     private final int[][] height = new int[SIZE][SIZE];
     private boolean[][] blocked = new boolean[SIZE][SIZE];
@@ -26,10 +28,11 @@ public class GameMap {
     }
 
     private void setupTerrain() {
+        final var random = new Random(1701L);
         IntStream.range(0, SIZE).forEach(x ->
                 IntStream.range(0, SIZE).forEach(y -> {
-				    height[x][y] = y % 3;
-				    blocked[x][y] = false;
+                    height[x][y] = random.nextInt(4);
+                    blocked[x][y] = false;
                     reserved[x][y] = false;
                 })
         );
@@ -46,20 +49,21 @@ public class GameMap {
     public void placeBuilding(final int x, final int y, final BuildingProperty building) {
         depictionHasChanged = true;
         var heightSum = 0;
-        for (var xy:building.getBlocking()) {
+        for (var xy : building.getBlocking()) {
             final var absoluteX = x + xy.getX();
             final var absoluteY = y + xy.getY();
             blocked[absoluteX][absoluteY] = true;
             reserved[absoluteX][absoluteY] = true;
             heightSum += height[absoluteX][absoluteX];
-        };
+        }
+        ;
         building.getAdditionalReserving().forEach(xy -> {
             final var absoluteX = x + xy.getX();
             final var absoluteY = y + xy.getY();
             reserved[absoluteX][absoluteY] = true;
         });
         var newHeight = Math.round((float) heightSum / (float) building.getBlocking().size());
-        for (var xy:building.getBlocking()) {
+        for (var xy : building.getBlocking()) {
             final var absoluteX = x + xy.getX();
             final var absoluteY = y + xy.getY();
             height[absoluteX][absoluteY] = newHeight;
@@ -68,32 +72,32 @@ public class GameMap {
 
     public RawModel generateRawModel(final Loader loader) {
         this.depictionHasChanged = false;
-        int count = SIZE * SIZE;
+        final var count = VERTEX_SIZE * VERTEX_SIZE;
         float[] vertices = new float[count * 3];
         float[] normals = new float[count * 3];
         float[] textureCoords = new float[count * 2];
-        int[] indices = new int[6 * (SIZE - 1) * (SIZE - 1)];
+        int[] indices = new int[6 * SIZE * SIZE];
         int vertexPointer = 0;
-        for (int x = 0; x < SIZE; x++) {
-            for (int y = 0; y < SIZE; y++) {
-                vertices[vertexPointer * 3] = (float) y / ((float) SIZE - 1) * SIZE;
-                vertices[vertexPointer * 3 + 1] = HEIGHT_FACTOR * height[x][y];
-                vertices[vertexPointer * 3 + 2] = (float) x / ((float) SIZE - 1) * SIZE;
+        for (int x = 0; x < VERTEX_SIZE; x++) {
+            for (int y = 0; y < VERTEX_SIZE; y++) {
+                vertices[vertexPointer * 3] = (float) y / ((float) SIZE) * VERTEX_SIZE;
+                vertices[vertexPointer * 3 + 1] = HEIGHT_FACTOR * vertexHeight(x, y);
+                vertices[vertexPointer * 3 + 2] = (float) x / ((float) SIZE) * VERTEX_SIZE;
                 final var normal = calculateNormal(x, y);
                 normals[vertexPointer * 3] = normal.x;
                 normals[vertexPointer * 3 + 1] = normal.y;
                 normals[vertexPointer * 3 + 2] = normal.z;
-                textureCoords[vertexPointer * 2] = (float) y / ((float) SIZE - 1);
-                textureCoords[vertexPointer * 2 + 1] = (float) x / ((float) SIZE - 1);
+                textureCoords[vertexPointer * 2] = (float) y / (float) SIZE;
+                textureCoords[vertexPointer * 2 + 1] = (float) x / (float) SIZE;
                 vertexPointer++;
             }
         }
         int pointer = 0;
-        for (int gz = 0; gz < SIZE - 1; gz++) {
-            for (int gx = 0; gx < SIZE - 1; gx++) {
-                int topLeft = (gz * SIZE) + gx;
+        for (int gz = 0; gz < SIZE; gz++) {
+            for (int gx = 0; gx < SIZE; gx++) {
+                int topLeft = (gz * VERTEX_SIZE) + gx;
                 int topRight = topLeft + 1;
-                int bottomLeft = ((gz + 1) * SIZE) + gx;
+                int bottomLeft = ((gz + 1) * VERTEX_SIZE) + gx;
                 int bottomRight = bottomLeft + 1;
                 indices[pointer++] = topLeft;
                 indices[pointer++] = bottomLeft;
@@ -106,17 +110,39 @@ public class GameMap {
         return loader.loadToVAO(vertices, textureCoords, normals, indices);
     }
 
+    private float vertexHeight(final int x, final int y) {
+        if (x == 0 && y == 0) {
+            return height[x][y];
+        } else if (x == 0 && y == SIZE) {
+            return height[x][y - 1];
+        } else if (x == 0) {
+            return ((float) height[x][y] + (float) height[x][y - 1]) / 2F;
+        } else if (y == 0 && x == SIZE) {
+            return height[x - 1][y];
+        } else if (y == 0) {
+            return ((float) height[x][y] + (float) height[x - 1][y]) / 2F;
+        } else if (x == SIZE && y == SIZE) {
+            return height[x - 1][y - 1];
+        } else if (x == SIZE) {
+            return ((float) height[x - 1][y] + (float) height[x - 1][y - 1]) / 2F;
+        } else if (y == SIZE) {
+            return ((float) height[x][y - 1] + (float) height[x - 1][y - 1]) / 2F;
+        } else {
+            return ((float) height[x][y] + (float) height[x][y - 1] + (float) height[x - 1][y] + (float) height[x - 1][y - 1]) / 4F;
+        }
+    }
+
     public boolean hasDepictionChanged() {
         return depictionHasChanged;
     }
 
     private Vector3f calculateNormal(final int x, final int y) {
-        final var xx = Math.max(1, Math.min(SIZE - 2, x));
-        final var yy = Math.max(1, Math.min(SIZE - 2, y));
-        final var heightL = height[xx - 1][yy];
-        final var heightR = height[xx + 1][yy];
-        final var heightD = height[xx][yy - 1];
-        final var heightU = height[xx][yy + 1];
+        final var xx = Math.max(1, Math.min(SIZE - 1, x));
+        final var yy = Math.max(1, Math.min(SIZE - 1, y));
+        final var heightL = vertexHeight(xx - 1, yy);
+        final var heightR = vertexHeight(xx + 1, yy);
+        final var heightD = vertexHeight(xx, yy - 1);
+        final var heightU = vertexHeight(xx, yy + 1);
         final var normal = new Vector3f(heightL - heightR, 2F, heightD - heightU);
         normal.normalise();
         return normal;
